0705c87c82ced0946cb725daeda152ff3b5606bf
oorur1 <ryosuke.t140819@gmail.com>
2025-08-25 15:45:04 +0900

    ユーザー名の送信とボタンのスタイリング

--- chatapp/src/main.js
+++ chatapp/src/main.js
@@ -2,6 +2,5 @@ import { createApp } from "vue"
 import "./style.css"
 import App from "./App.vue"
 import router from "./router"
-import vuetify from "./plugins/vuetify"
 
-createApp(App).use(vuetify).use(router).mount("#app")
+createApp(App).use(router).mount("#app")
