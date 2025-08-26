6dfa780e332607c6a56744c7f144724e0afdc15a
tutagebo <koudai.nattoufu@gmail.com>
2025-08-26 11:37:23 +0900

    add socket event

--- chatapp/socket_event/index.js
+++ chatapp/socket_event/index.js
@@ -1,3 +1,7 @@
+const messageList = [];
+const taskList = [];
+
+
 export default (io, socket) => {
   // 入室メッセージをクライアントに送信する
   socket.on("enterEvent", (data) => {
@@ -13,4 +17,49 @@ export default (io, socket) => {
   socket.on("publishEvent", (data) => {
     io.sockets.emit("publishEvent", data)
   })
+
+  // タスクを登録
+  socket.on("registTask", (data) => {
+    io.sockets.emit("registTask", data)
+  })
+
+  // タスクを削除
+  socket.on("deleteTask", (data) => {
+    io.sockets.emit("deleteTask", data)
+  })
+
+  // タスク担当を変更
+  socket.on("changeAsignee", (data) => {
+    io.sockets.emit("changeAsignee", data)
+  })
+
+  // タスク期限を変更
+  socket.on("changeDuration", (data) => {
+    io.sockets.emit("changeDuration", data)
+  })
+
+  // タスク開始日を変更
+  socket.on("changeStartDate", (data) => {
+    io.sockets.emit("changeDuration", data)
+  })
+
+  // タスク担当を完了
+  socket.on("completeTask", (data) => {
+    io.sockets.emit("completeTask", data)
+    JSON.stringify
+  })
+}
+
+class TaskItem{
+  /** @type {string} タスク名 */
+  #content;
+  /** @type {} 開始日 */
+  #startDate;
+  /** @type {number} 期間 */
+  #duration;
+  /** @type {string} 担当者 */
+  #asignee;
+  /** @type {bool}  */ // statusにして{ "todo" | "inProgress" | "done" }みたいなのもあり
+  #isDone
+  /** @type {number} */
 }
